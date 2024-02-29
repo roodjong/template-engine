7e8303f7b057fca69e86482b7c5f41125be90f0b
author name <test@test.test>
2024-02-29 15:48:10 +0100

    4

--- js/aankondiging.js
+++ js/aankondiging.js
@@ -1,44 +1,5 @@
-
-const textInput = document.getElementById("textInput");
-const canvas = document.getElementById("canvas");
-const context = canvas.getContext('2d');
-
 let backgroundImage = new DraggableResizableObject(canvas, "imageSize", document.createElement('img'), 0, 0, 0, 0);
 let topText = new TextDrawable("titelText", "10px Bebas Neue", 0.2, "white", context, 0, 0, 2000, 500);
 
-let layers = [backgroundImage, "frame.png", topText];
-
-
-
-document.getElementById('imageInput').addEventListener('change', handleImageUpload);
-document.addEventListener('DOMContentLoaded', onStartup);
-
-function onStartup(){
-    fixLayerIndirection();
-
-    drawTemplate();
-}
-
-
-
-function handleImageUpload(event) {
-  const file = event.target.files[0];
-
-  if (file) {
-    const reader = new FileReader();
-
-    reader.onload = function (e) {
-        backgroundImage.img.src = e.target.result;
-        backgroundImage.size.width = backgroundImage.img.width;
-        backgroundImage.size.height = backgroundImage.img.height;
-        backgroundImage.position.x = 0;
-        backgroundImage.position.y = 0;
-        drawTemplate();
-    };
-
-    reader.readAsDataURL(file);
-  }
-}
-
-
-
+//Define layers here
+let layers = [backgroundImage, "templateImages/Aankondiging/frame.png", topText];
--- js/shared.js
+++ js/shared.js
@@ -1,3 +1,6 @@
+const canvas = document.getElementById("canvas");
+const context = canvas.getContext('2d');
+
 function fixLayerIndirection() {
     layers = layers.map(layer => {
         if (typeof layer === 'string') {
@@ -39,4 +42,35 @@ function download(template_name) {
 
     // Trigger a click on the link to start the download
     link.click();
+}
+
+
+document.getElementById('imageInput').addEventListener('change', handleImageUpload);
+function handleImageUpload(event) {
+    const file = event.target.files[0];
+  
+    if (file) {
+      const reader = new FileReader();
+  
+      reader.onload = function (e) {
+          backgroundImage.img.src = e.target.result;
+          backgroundImage.size.width = backgroundImage.img.width;
+          backgroundImage.size.height = backgroundImage.img.height;
+          backgroundImage.position.x = 0;
+          backgroundImage.position.y = 0;
+          drawTemplate();
+      };
+  
+      reader.readAsDataURL(file);
+    }
+  }
+
+  
+
+document.addEventListener('DOMContentLoaded', onStartup);
+
+function onStartup(){
+    fixLayerIndirection();
+
+    drawTemplate();
 }
