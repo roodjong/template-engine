8aa87a4d6d832c5dfb1a53b02cef57840e0b2fc8
Marcel Bostelaar <marcel.bostelaar@gmail.com>
2024-03-01 12:24:45 +0100

    Hopefully fixed groepsname misallignment

--- js/aankondiging.js
+++ js/aankondiging.js
@@ -2,7 +2,7 @@ let backgroundImage = new DraggableResizableObject(canvas, "imageSize", document
 let topText = new TextDrawable({font: "Bebas Neue", linespacing: 0.2, color: "white", allCaps: true, context:context,
     xPosition: 30, yPosition: 100, maxWidth:1940, maxHeight:400, inputID:"titelText"});
 let groepsnaam = new TextDrawable({font: "BebasNeueBook", linespacing: 0, color: "white", allCaps : true, context:context,
-    xPosition: 243, yPosition: 1840, doDynamicSize: false, fontSize: 72.5, doCenter: false, inputID: "groepsnaam",});
+    xPosition: 243, yPosition: 1843, doDynamicSize: false, fontSize: 70.5, doCenter: false, inputID: "groepsnaam",});
 let ondertitel = new TextDrawable({font: "BebasNeueBook", linespacing: 0, color: "white", allCaps : true, context:context,
     xPosition: 0, yPosition: 600, maxWidth: 2000, doDynamicSize: false, fontSize: 72.5, inputID: "onderTitel"});
 
@@ -11,7 +11,7 @@ let layers = [
     backgroundImage, 
     new ImgWrapper("templateImages/Aankondiging/rechtsMultiply.png", "multiply"), 
     "templateImages/Aankondiging/frame.png", 
-    topText, 
+    topText,
     groepsnaam,
     ondertitel
 ];
--- js/tekstbericht.js
+++ js/tekstbericht.js
@@ -1,7 +1,7 @@
 let topText = new TextDrawable({font: "Bebas Neue", linespacing: 0.2, color: "rgb(194, 0, 10)", context: context, 
     xPosition: 30, yPosition: 95, maxWidth:1940, maxHeight:310, inputID:"titelText"});
 let groepsnaam = new TextDrawable({font: "BebasNeueBook", linespacing: 0, color: "white", allCaps : true, context: context,
-    xPosition: 243, yPosition: 1840, doDynamicSize: false, fontSize: 72.5, doCenter: false, inputID: "groepsnaam"});
+    xPosition: 243, yPosition: 1843, doDynamicSize: false, fontSize: 70.5, doCenter: false, inputID: "groepsnaam"});
 let oproepTekst = new TextDrawable({font: "BebasNeueSEEB", linespacing: 0.5, color: "black", context: context,
 xPosition: 50, yPosition: 600, maxWidth: 1890, doDynamicSize: false, fontSize: 75, inputID: "oproepTekst"})
 document.addEventListener("DOMContentLoaded", ()=>oproepTekst.centerSelfHorizontal(canvas))
