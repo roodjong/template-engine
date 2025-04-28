73e68aad26e25f614d081a6b57074e46221b137e
Splinter Suidman <splinter@mannenopdemaan.nl>
2025-04-28 22:42:05 +0200

    Update Socialisten to RSP, add ROOD and RSP template

---
+++ js/rsproodaankondiging.js
@@ -0,0 +1,50 @@
+const heightToWidthRatio = 5/4;
+
+const templateWidth = 2000;
+const templateHeight = heightToWidthRatio * templateWidth;
+canvas.width = templateWidth;
+canvas.height = templateHeight;
+
+let backgroundImage = new DraggableResizableObject(canvas, "imageSize", document.createElement('img'), 0, 0, 0, 0);
+backgroundImage.makeDraggable(canvas);
+let toptitel = new TextDrawable({font: "LibreFranklinItalic", linespacing: 0, color: "white", allCaps: true, context:context,
+    xPosition: 0, yPosition: 1690, maxWidth:2000, inputID:"topText", fontSize: 85, doDynamicSize: false, fontWeight: "bold"});
+let titel = new TextDrawable({font: "LibreFranklin", linespacing: 0.05, color: "white", allCaps: true, context:context, letterSpacing: 5,
+    xPosition: 100, yPosition: 2250, maxWidth:1800, maxHeight:230, inputID:"titelText", doBottomYPosition: true, fontWeight: "800",
+    doDynamicSize: false, fontSize:145, useDynamicLineHeight: false, textBaseline: "alphabetic"});
+let ondertitel = new TextDrawable({font: "LibreFranklin", linespacing: 0.5, color: "white", allCaps: true, context:context,
+    xPosition: 100, yPosition: 2250, maxWidth:1800, inputID:"onderTitel", letterSpacing: 10, doDynamicSize: false, fontSize: 75});
+let logos = new ImgWrapper("templateImages/RSPROODAankondiging/logos.png");
+let balkje = new ImgWrapper("templateImages/RSPROODAankondiging/roodbalkje.png");
+balkje.setPosition(0, 500); // 500 for changed aspect ratio
+
+toptitel.lockXPosition();
+toptitel.makeDraggable();
+titel.lockXPosition();
+titel.makeDraggable();
+ondertitel.lockXPosition();
+ondertitel.makeDraggable();
+
+const shadowColor = "rgb(0, 0, 0, 30%)";
+const shadowBlur = 16;
+const xOffset = 10;
+const yOffset = 10;
+
+balkje.setShadow(shadowColor, shadowBlur, xOffset, yOffset);
+toptitel.setShadow(shadowColor, shadowBlur, xOffset, yOffset);
+titel.setShadow(shadowColor, shadowBlur, xOffset, yOffset);
+ondertitel.setShadow(shadowColor, shadowBlur, xOffset, yOffset);
+
+const fade = new ImgWrapper("templateImages/RSPROODAankondiging/fade.png", "multiply");
+fade.setPosition(0, templateHeight - 2000);
+
+//Define layers here
+let layers = [
+    backgroundImage, 
+    fade,
+    logos,
+    balkje,
+    toptitel,
+    titel,
+    ondertitel
+];
